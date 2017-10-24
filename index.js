c4e35a79afed4146367d46ec82a12d163ba98e52
Jeremy Daly <jeremy@jeremydaly.com>
2017-10-24 19:08:18 -0400

    bugfix: default stage not resolving correctly

--- index.js
+++ index.js
@@ -2,28 +2,26 @@
 
 class ServerlessPlugin {
   constructor(serverless, options) {
-    this.serverless = serverless;
-    this.options = options;
-    this.stage = this.options.stage ? this.options.stage : this.serverless.service.provider.stage
-
+    this.serverless = serverless
+    this.options = options
     this.hooks = {
       'before:package:initialize': this.checkStage.bind(this),
       'before:deploy:function:initialize': this.checkStage.bind(this)
-    };
-
-  }
-
-  checkStage() {
-    if (!this.serverless.service.custom.stages || !Array.isArray(this.serverless.service.custom.stages)) {
-      throw new Error(`A "stages" array must be defined in your serverless.yml's "custom" section.`)
-    } else {
-      if (this.serverless.service.custom.stages.indexOf(this.stage) === -1) {
-        throw new Error(`'${this.stage}' is not a valid deployment stage. Add it to your serverless.yml's "custom.stages" section.`)
-      }
     }
   }
 
+  checkStage() {
+    let stage = this.options.stage ? this.options.stage : this.serverless.service.provider.stage
 
-}
+    if (!this.serverless.service.custom.stages || !Array.isArray(this.serverless.service.custom.stages)) {
+      throw new Error(`A "stages" array must be defined in your serverless.yml's "custom" section.`)
+    } else {
+      if (this.serverless.service.custom.stages.indexOf(stage) === -1) {
+        throw new Error(`'${stage}' is not a valid deployment stage. Add it to your serverless.yml's "custom.stages" section.`)
+      }
+    }
+  } // end checkStage
+
+} // end class
 
 module.exports = ServerlessPlugin;
